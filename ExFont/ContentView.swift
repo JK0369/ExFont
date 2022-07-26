4b4cbda5cfa4da4b3efd9fc33d2269689e91c884
jake.k <jake.k@hpcnt.com>
2022-07-26 17:51:23 +0900

    Initial Commit

---
+++ ExFont/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ExFont
+//
+//  Created by Jake.K on 2022/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ExFont/ExFontApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExFontApp.swift
+//  ExFont
+//
+//  Created by Jake.K on 2022/07/26.
+//
+
+import SwiftUI
+
+@main
+struct ExFontApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
